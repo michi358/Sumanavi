afdecb9f6f4e692ecbc9ffc4368923e34b8a1b9c
EC2 Default User <ranm358m@gmail.com>
2024-04-06 05:31:41 +0000

    [Update]post/一覧、編集機能の追加

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,6 +1,7 @@
 class Public::PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def new
@@ -21,9 +22,13 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update (post_params)
+    redirect_to post_path(@post.id)
   end
 
   def destroy
