c2e22aa3b66fc5eb57f649970b018b709e10938d
EC2 Default User <ranm358m@gmail.com>
2024-04-11 15:04:53 +0000

    [Add]admin/devise機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  
+
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
   # エンドユーザー
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+
+
   scope module: :public do
     root to: 'homes#top'
     get '/about' => "homes#about", as: 'about'
@@ -17,9 +21,14 @@ Rails.application.routes.draw do
       end
     end
   end
-  
+
    # ゲストログイン
   devise_scope :user do
     post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
+
+  namespace :admin do
+    resources :users, only: [:index]
+  end
+
 end
