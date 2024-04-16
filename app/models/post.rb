0d679dde1abbfbe8e24db1bf8a83d9cc7da499e0
EC2 Default User <ranm358m@gmail.com>
2024-04-16 11:40:46 +0000

    [Add]いいね機能、管理者側へ表示追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_rich_text :content
- 
   has_many :post_comments, dependent: :destroy
   belongs_to :genre
+  has_many :favorites, dependent: :destroy
   
   validates :title, presence: true
   validates :content, presence: true
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  
 end
