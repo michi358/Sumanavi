b37d52a3748257d29e0d26381b0020ebcb492fe8
EC2 Default User <ranm358m@gmail.com>
2024-04-17 10:33:34 +0000

    [Update]フォロー、フォロワー一覧追加

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -1,7 +1,6 @@
 class Public::RelationshipsController < ApplicationController
   
   
-  
   def create
     user = User.find(params[:user_id])
     current_user.follow(user)
@@ -13,15 +12,15 @@ class Public::RelationshipsController < ApplicationController
     current_user.unfollow(user)
     redirect_to  request.referer
   end
-  
+  # フォロー中
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings.page(params[:page])
   end
-  
+  # フォロワー
   def followers
-    user = User.find(params[:user_id])
-    @users = user.followers
+    @user = User.find(params[:user_id])
+    @users = @user.followers.page(params[:page])
   end
   
 end
