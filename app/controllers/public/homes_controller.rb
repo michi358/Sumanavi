a79e7d9f0397ce86d3148c43f14bcfe38e6def83
EC2 Default User <ranm358m@gmail.com>
2024-04-21 12:43:43 +0000

    topのレイアウト

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,15 @@ class Public::HomesController < ApplicationController
   def top
     @posts = Post.all
     @genres = Genre.all
+    @tags = Tag.all.page(params[:page]).per(6)
     @post_favorite_ranks = @posts.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
+    # @users = User.all
+    # @followe_ranks = User.find(Relationship.group(:follower_id).order('count(follower_id)desc').pluck(:follower_id))
+    # @user = @followe_ranks.user   
+    
+    follower_counts = Relationship.group(:followed_id).count
+    sorted_user_ids = follower_counts.keys.sort_by { |user_id| -follower_counts[user_id] }
+    @top_users = User.where(id: sorted_user_ids.take(3))
   end
 
   def about
