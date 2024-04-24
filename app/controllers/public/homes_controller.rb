ef55e3fa4a41fd0dd70a2501fd8101db2b6a2930
EC2 Default User <ranm358m@gmail.com>
2024-04-24 09:59:54 +0000

    実装フェーズ3完了

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -4,7 +4,7 @@ class Public::HomesController < ApplicationController
     @genres = Genre.all
     @tags = Tag.all.page(params[:page]).per(6)
     # @post_favorite_ranks = @posts.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
-    @post_favorite_ranks = @posts.published.joins(:favorites).group(:id).order('COUNT(favorites.id) DESC').limit(8)
+    @post_favorite_ranks = @posts.published.joins(:favorites).group(:id).order('COUNT(favorites.id) DESC').limit(3)
     @followe_ranks = User.left_joins(:followers).group(:id).order('COUNT(users.id) DESC').limit(3)
   end
   
