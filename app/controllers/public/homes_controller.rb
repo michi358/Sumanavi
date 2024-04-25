fe5e289bbb4cab67a138b9482f074d1e3c5d8ab7
EC2 Default User <ranm358m@gmail.com>
2024-04-25 07:59:56 +0000

    topページコントローラーの修正（公開投稿のみのタグを取り出す）

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,12 +2,12 @@ class Public::HomesController < ApplicationController
   def top
     @posts = Post.published.all
     @genres = Genre.all
-    @tags = Tag.all.page(params[:page]).per(6)
+    @tags = Tag.joins(:posts).merge(Post.published).distinct.page(params[:page]).per(6)
     # @post_favorite_ranks = @posts.find(Favorite.group(:post_id).order('count(post_id) desc').pluck(:post_id))
     @post_favorite_ranks = @posts.published.joins(:favorites).group(:id).order('COUNT(favorites.id) DESC').limit(3)
     @followe_ranks = User.left_joins(:followers).group(:id).order('COUNT(users.id) DESC').limit(3)
   end
-  
+
   def next_page
     @tags = Tag.page(params[:page])
     respond_to do |format|
